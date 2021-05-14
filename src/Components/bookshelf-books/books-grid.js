6346d99ebe309120a9391db51dce11d1d681a6d9
miriamsafwat2 <miriam.safwat@gmail.com>
2021-05-15 00:24:18 +0200

    fix: undefined links

--- src/Components/bookshelf-books/books-grid.js
+++ src/Components/bookshelf-books/books-grid.js
@@ -7,8 +7,9 @@ class BooksGrid extends React.Component {
     return (
       <ol className="books-grid">
         {this.props.readingList.map((b) => {
+          let link = ''; if(b.imageLinks !== undefined) link = b.imageLinks.thumbnail
           return <li key={b.id}>
-              <Book book={b} url={b.imageLinks.thumbnail} title={b.title} authors={b.authors} onReadStatusChange={this.props.onReadStatusChange} />
+              <Book book={b} url= {link} title={b.title} authors={b.authors} onReadStatusChange={this.props.onReadStatusChange} />
             </li>;
         })}
       </ol>
